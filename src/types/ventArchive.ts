73712924af4c933155b553864936f563ac9c0d8f
guinetik <guinetik@gmail.com>
2026-03-30 01:15:01 -0300

    feat(dan): extend archives with storedKg/lastChargedSol; add co2-gas and methane-gas items

--- src/types/ventArchive.ts
+++ src/types/ventArchive.ts
@@ -6,4 +6,8 @@ export interface ArchivedVent {
   /** World position where the vent GLB should be placed on restore. */
   x: number
   z: number
+  /** kg of gas accumulated since deploy. Undefined until first dock. */
+  storedKg?: number
+  /** Sol at which storedKg was last calculated. Defaults to placedSol on first dock. */
+  lastChargedSol?: number
 }
